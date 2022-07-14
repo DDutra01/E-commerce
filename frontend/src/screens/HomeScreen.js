cf56071a10e187226c35f1658ac18c295529902d
Diego Dutra <digo7792gmail.com>
2022-07-14 18:28:14 -0300

    starting backend, fetchapi

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,23 @@
-import data from "../data";
+//import data from "../data";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function HomeScreen() {
+  const [products, setproducts] = useState([]);
+  useEffect(() => {
+    const fecthData = async () => {
+      const result = await axios.get("/api/products");
+      setproducts(result.data);
+    };
+    fecthData();
+  }, []);
+
   return (
     <div>
       <h1>Feacture Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
